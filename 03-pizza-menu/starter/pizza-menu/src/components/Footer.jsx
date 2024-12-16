d6afbcd0f37d46cb2f545d9884f15c4998e2749f
arpit mishra <arpit.mishra.out@gmail.com>
2024-12-16 23:05:02 +0530

    Built a Profile card challenge using props and components

--- 03-pizza-menu/starter/pizza-menu/src/components/Footer.jsx
+++ 03-pizza-menu/starter/pizza-menu/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const isShopOpen = function ()
 {
     const hours = new Date().getHours();
-    return hours >= 10 && hours <= 22 ? ' We are currently open! ' : 'We are closed for now,look at our sehdule.';
+    return hours >= 10 && hours < 22 ? ' We are currently open! ' : 'We are closed for now,look at our sehdule.';
 }
 
 export default function Footer()
