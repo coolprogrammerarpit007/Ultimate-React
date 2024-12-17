f99194622f7e1b7fd74816af8975e122209bb2fd
arpit mishra <arpit.mishra.out@gmail.com>
2024-12-17 22:10:21 +0530

    Conditional Rendering

--- 03-pizza-menu/starter/pizza-menu/src/components/Footer.jsx
+++ 03-pizza-menu/starter/pizza-menu/src/components/Footer.jsx
@@ -1,10 +1,13 @@
 const isShopOpen = function ()
 {
     const hours = new Date().getHours();
-    return hours >= 10 && hours < 22 ? ' We are currently open! ' : 'We are closed for now,look at our sehdule.';
+    return hours >= 10 && hours < 24 ? true : false;
 }
 
 export default function Footer()
 {
-    return <footer className = 'footer'> { new Date().toLocaleTimeString() } { isShopOpen() }</footer>
+    return <footer className = 'footer'> { isShopOpen() && <div className="order">
+        <p>We are open Untill 12::00 AM. Come visit us order online.</p>
+        <button className="btn">Order</button>
+    </div> }</footer>
 }
--- 03-pizza-menu/starter/pizza-menu/src/components/Menu.jsx
+++ 03-pizza-menu/starter/pizza-menu/src/components/Menu.jsx
@@ -5,11 +5,14 @@ export default function Menu()
     return (
         <main className='menu'>
             <h2>Our Menu</h2>
-            <ul className='pizzas'>
-                {pizzaData.map((pizza) => {
-                    return (<Pizza name={pizza.name} ingredients={pizza.ingredients} price={pizza.price} img={pizza.photoName} key={pizza.name} />);
-                })}
-            </ul>
+            {
+                pizzaData.length > 0 &&
+                    <ul className='pizzas'>
+                        {pizzaData.map((pizza) => {
+                            return (<Pizza name={pizza.name} ingredients={pizza.ingredients} price={pizza.price} img={pizza.photoName} key={pizza.name} />);
+                        })}
+                    </ul>
+}
         </main>
     )
 }
